83328e001d6db18f64d621bbbbd341ae4c42f246
superwhy <natarajaninbx@gmail.com>
2024-07-18 23:09:16 -0400

    updated with curl

--- update-host.sh
+++ update-host.sh
@@ -4,16 +4,14 @@ HOST_FILE=/etc/hosts
 TMP_HOST_FILE=/tmp/etc-hosts
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 
-#rm -rf $TMP_HOST_FILE
+# Download the file from GitHub
+curl -H "Cache-Control: no-cache" -L -o $TMP_HOST_FILE https://raw.githubusercontent.com/natarajaninbox/block-domains/main/etc-hosts
 
-#wget  -q https://raw.githubusercontent.com/natarajaninbox/block-domains/main/etc-hosts -O $TMP_HOST_FILE || exit 1
-
-curl -o $TMP_HOST_FILE https://raw.githubusercontent.com/natarajaninbox/block-domains/main/etc-hosts
-
-
-
-
-
+# Check if the temporary file was successfully downloaded
+if [[ ! -f $TMP_HOST_FILE ]]; then
+  echo "Failed to download the file."
+  exit 1
+fi
 
 # Get the checksum of the current file
 checksum=$(md5 -q $HOST_FILE)
@@ -21,12 +19,14 @@ checksum=$(md5 -q $HOST_FILE)
 # Get the checksum of the file on GitHub
 github_checksum=$(md5 -q $TMP_HOST_FILE)
 
-echo $checksum
-echo $github_checksum
+echo "Local checksum: $checksum"
+echo "GitHub checksum: $github_checksum"
 
 # If the checksums don't match, then update the file
 if [[ "$checksum" != "$github_checksum" ]]; then
   echo "Updating local etc-hosts file"
-  cp $HOST_FILE $HOST_FILE.$TIMESTAMP.bak  
+  cp $HOST_FILE $HOST_FILE.$TIMESTAMP.bak
   mv $TMP_HOST_FILE $HOST_FILE
+else
+  echo "No update needed. The files are identical."
 fi
